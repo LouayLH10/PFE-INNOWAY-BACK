783cd4964cd65960ed4d2e59db3fcdc32abf0781
LouayLH10 <hcinlouay@gmail.com>
2026-06-27 10:51:38 +0100

    cors

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,8 @@ import { join } from 'path';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 app.enableCors({
-  origin: [
-    'http://localhost:3000',
-    'https://ton-frontend.vercel.app',
-  ],
+  origin: true,
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
 });
   await app.listen(process.env.PORT ?? 3200);
 }
